95d6e8b30353560120c151a3c1ec094fc70d6a99
Harrison Ballard <harrisonwballard@gmail.com>
2020-11-02 19:40:05 -0500

    fix on challenge 2 with rel path

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
+// app.get('/', function(req, res) {
+//     res.send('Hello Express');
+// })
 
-app.get('/', function(req, res) {
-    res.send('Hello Express');
-})
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+    });
+
 
  module.exports = app;
