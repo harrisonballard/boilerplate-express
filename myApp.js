ecbb020caee40c5882ada27f7ac42d5e658e9124
Harrison Ballard <harrisonwballard@gmail.com>
2020-11-02 19:53:41 -0500

    added app.use

--- myApp.js
+++ myApp.js
@@ -5,11 +5,10 @@ var app = express();
 //     res.send('Hello Express');
 // })
 
-
-
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
     });
 
+app.use(__dirname + '/public', express.static())
 
  module.exports = app;
